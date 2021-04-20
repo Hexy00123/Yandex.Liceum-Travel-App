0ba43e9a227f20f3579195ee9f22205a43354ed7
Angelika2707 <angelika270702@gmail.com>
2021-04-20 16:44:09 +0400

    сохраняю промежуток2

--- main.py
+++ main.py
@@ -3,13 +3,8 @@ from logic import StartWindow
 from PyQt5.QtWidgets import QApplication
 
 
-def except_hook(cls, exception, traceback):
-    sys.__excepthook__(cls, exception, traceback)
-
-
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     ex = StartWindow()
     ex.show()
-    sys.excepthook = except_hook
     sys.exit(app.exec_())
